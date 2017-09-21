c85df987ed435813f4886a89a40e865392298f01
hinst <hinstance@yandex.ru>
2017-09-22 00:12:04 +0300

    Added w3.css

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 appSubDir = "pushy";
 webs = $loader.loadmodule(appSubDir + "/webs", $loader);
 hint = $loader.loadmodule(appSubDir + "/hint", $loader);
+response = hint.readFileContent(appSubDir + "/layout.html");
+$print(response);
